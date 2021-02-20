9e6488d65aedc391d16342052f96b03011f659e2
AymanAlshaikh <ayman159@live.com>
2021-02-20 14:24:38 +0300

    updated

--- src/components/CategoryDetail.js
+++ src/components/CategoryDetail.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { Redirect, useParams } from "react-router-dom";
 import IngredientList from "./IngredientList";
 import { ItemWrapper } from "../styles";
+import IngredientItem from "./IngredientItem";
 //import UpdateButton from "./buttons/UpdateButton";
 const CategoryDetail = () => {
   const allIngredients = useSelector(
@@ -17,15 +18,18 @@ const CategoryDetail = () => {
   );
 
   if (!category) return <Redirect to="/categories" />;
-  const ingredients = category.ingredients.map((ingredient) =>
-    allIngredients.find((_ingredient) => _ingredient.id === ingredient.id)
-  );
+  // const ingredients = category.ingredients.map(
+  //   (ingredient) => <IngredientItem /> //ingredients={ingredient} />
+  //   //allIngredients.find((_ingredient) => _ingredient.id === ingredient.id)
+  // );
   //change line 21 to bring the ingredient list
   return (
     <ItemWrapper>
+      {/* {ingredients} */}
       <h4>{category.name}</h4>
       <img src={category.image} alt={category.name} />
-      <IngredientList ingredients={ingredients} />
+      <IngredientList />
+      {/* ingredients={ingredients} /> */}
       {/* <UpdateButton ingredients={category.ingredients} /> */}
       <Link to={`/categories/${category.id}/ingredients/new`}>
         <BsFillPlusSquareFill className="float-right" size="2em" />
--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -4,25 +4,29 @@ import { BsFillPlusSquareFill } from "react-icons/bs";
 // Components
 import IngredientItem from "./IngredientItem";
 // Data
-
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const IngredientList = (props) => {
+  let [id, setId] = useState([]);
+  let ings = [];
+  ings.push(id);
+  console.log(ings);
   const ingredients = useSelector(
     (state) => state.ingredientReducer.ingredient
   );
   const ingredientList = ingredients.map((ingredient) => (
-    <IngredientItem ingredient={ingredient} key={ingredient.id} />
+    <IngredientItem ingredient={ingredient} key={ingredient.id} setId={setId} />
   ));
   return (
     <div className="container">
       <h1>Food Ingredients</h1>
       <ListWrapper>
         {ingredientList}
-        <Link to="/ingredients/new">
+        {/* <Link to="/ingredients/new">
           <BsFillPlusSquareFill size="2em" />
-        </Link>
+        </Link> */}
       </ListWrapper>
     </div>
   );
